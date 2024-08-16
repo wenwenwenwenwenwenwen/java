a8f0e6af86a91f3892bd3cc75c4928f589410363
wenwenwenwenwenwenwen <2936283507@qq.com>
2024-08-16 22:21:29 +0800

    Update PortScanner1.java

--- PortScanner1.java
+++ PortScanner1.java
@@ -94,7 +94,7 @@ public class PortScanner1 {
                     connection.setReadTimeout(3000);
                     int responseCode = connection.getResponseCode();
                     if (responseCode == 200) {
-                        Thread.sleep(2500); // 等待3秒
+                        Thread.sleep(3000); // 等待3秒
                         try {
                             URL url2 = new URL(String.format(CMD_URL_FORMAT, exePort, port));
                             connection2 = (HttpURLConnection) url2.openConnection();
